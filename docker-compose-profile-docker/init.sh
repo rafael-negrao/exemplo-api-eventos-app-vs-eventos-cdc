88ead983557d9c19a7fd5ac44e18c1624606744b
rafael-negrao <rafael.negrao@adelfo.com.br>
2023-04-01 15:23:02 -0300

    estabilizando o script init para carregar o docker compose do server

--- docker-compose-profile-docker/init.sh
+++ docker-compose-profile-docker/init.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+echo "preparando para recupera o hostname"
+
 HOSTNAME_COMMAND=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
 export HOSTNAME_COMMAND
 
-docker compose -d
+echo "HOSTNAME_COMMAND = $HOSTNAME_COMMAND"
+
+echo "preparando para carregar o docker-compose"
+
+docker compose up -d
